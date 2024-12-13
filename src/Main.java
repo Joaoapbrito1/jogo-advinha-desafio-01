8cfc3975ca1fd703cb400ee62dc45d6d1ea8b7b1
Joaoapbrito1 <joao.brito@zup.com.br>
2024-12-13 15:24:51 -0300

    Feat: Implementa o menu do programa.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        // Menu de Niveis
+        System.out.println("=========================== JOGO DO ADVINHA ===============================");
+        System.out.println("Niveis de dificudade do jogo:");
+        System.out.println("1 - Nivel Fácil (Nesse nivel você tem que acertar um numero entre 1 e 10)");
+        System.out.println("2 - Nivel Médio (Nesse nivel você tem que acertar um numero entre 1 e 50)");
+        System.out.println("3 - Nivel Dificil (Nesse nivel você tem que acertar um numero entre 1 e 100)");
+        System.out.println("Digite o número correspondente ao nivel desejado:");
+        int nivel = scanner.nextInt();
     }
-}
+    }
